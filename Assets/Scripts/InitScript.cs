aab4e5e55d37950857627d862514d25444c4218c
mux <mux@darla.local>
2014-01-25 17:41:16 +0100

    added level loading, level reloading.

---
+++ Assets/Scripts/InitScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (GameObject.Find ("MainGameController(Clone)") == null) {
+			Instantiate(Resources.Load("MainGameController")); 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/MainGameControlScript.cs
+++ Assets/Scripts/MainGameControlScript.cs
@@ -1,20 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MainGameControlScript : MonoBehaviour {
 
+	public Dictionary<int, string> levels = new Dictionary<int, string>();
+	public int currentLevel;
 
 	// Use this for initialization
 	void Start () {
-	
+		// Define level: "SceneName" dictionary here.
+		this.levels.Add (1, "theGrid");
+		// this.levels.Add (2, "<SCENE_NAME>");
+		// this.levels.Add (3, "<SCENE_NAME>");
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.R)) {
-			// Reload the level.
-			Debug.Log("Loading Scene");
-			Application.LoadLevel("theGrid");
+			// Reload the current level.
+			if (this.currentLevel == 0) {
+				this.currentLevel = 1;
+			}
+			string currentLevel;
+			this.levels.TryGetValue(this.currentLevel, out currentLevel);
+			Application.LoadLevel(currentLevel);
 		}
+		if (Input.GetKeyDown(KeyCode.N)) {
+			// Load the next level or if max level reached.
+			if (this.currentLevel == 0) {
+				this.currentLevel = 1;
+			}
+			this.currentLevel = this.currentLevel + 1;
+			string nextLevel;
+			this.levels.TryGetValue(this.currentLevel, out nextLevel);
+			if (nextLevel == null) {
+				this.currentLevel = 1;
+				Application.LoadLevel("theGrid");
+			} else {
+				Application.LoadLevel(nextLevel);
+			}
+		}
+	}
+
+	void Awake() {
+		// Persist the MainGameController(Clone) object while loading scenes.
+		DontDestroyOnLoad(this.gameObject);
 	}
 }
