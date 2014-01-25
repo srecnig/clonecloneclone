a68f4b5253d9c5be608112516be5a817115e7b71
mux <mux@darla.local>
2014-01-25 14:57:07 +0100

    added shooting to clones.

--- Assets/Scripts/ShootScript.cs
+++ Assets/Scripts/ShootScript.cs
@@ -3,20 +3,23 @@ using System.Collections;
 
 public class ShootScript : MonoBehaviour {
 
-	public Transform playerTransform;
+	public Transform cloneTransform;
 
 	// Use this for initialization
 	void Start () {
-		this.playerTransform = GameObject.Find ("clone").transform;
+		this.cloneTransform = this.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space)) {
+			// Debug.Log("SHOOT!" + this.gameObject.tag);
 			// instantiate arrow prefab
 			GameObject arrow = Instantiate(Resources.Load("Arrow")) as GameObject; 
-			arrow.transform.position = this.playerTransform.position;
-			arrow.transform.rotation = this.playerTransform.rotation;
+			Vector3 pos = this.cloneTransform.position;
+			arrow.transform.position = pos;
+			arrow.transform.rotation = this.cloneTransform.rotation;
+			arrow.transform.Translate (0, 0.8F, 0);
 			arrow.AddComponent("ArrowScript");
 		}
 	}
