505bf5f52d9f1109af81cb9ef785136fb7e448ae
mux <mux@darla.local>
2014-01-25 13:59:30 +0100

    added arrow collision

--- Assets/Scripts/ArrowScript.cs
+++ Assets/Scripts/ArrowScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ArrowScript : MonoBehaviour {
 
-	public float shootSpeed = 15.0F;
+	public float shootSpeed = 5.0F;
 
 	// Use this for initialization
 	void Start () {
@@ -13,5 +13,14 @@ public class ArrowScript : MonoBehaviour {
 	void Update () {
 		float move = Time.deltaTime * shootSpeed;
 		this.transform.Translate (0, move, 0);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		string tag = collision.gameObject.tag;
+		if (tag == "Neutral") {
+			Debug.Log("COLLISION with: " +  collision.gameObject.tag);
+			Destroy(this.gameObject);
+		}
+
 	}
 }
