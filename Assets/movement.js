1a1bc681c0880b96faf929820edb76c7895885ac
LieroLarryfier <steindlmatthias@gmx.at>
2014-01-25 10:14:54 +0100

    alpha
    Don`t hurt yourself

---
+++ Assets/movement.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+if (Input.GetKeyDown(KeyCode.W)) {
+		transform.Translate(0,1,0);
+	}
+	if (Input.GetKeyDown(KeyCode.A)) {
+		transform.Translate(-1,0,0);
+	}
+	if (Input.GetKeyDown(KeyCode.S)) {
+		transform.Translate(0,-1,0);
+	}
+	if (Input.GetKeyDown(KeyCode.D)) {
+		transform.Translate(1,0,0);
+	}
+}
