bea62337501c2fce44e7a83484fffcdd800464f5
Apocrypt <westreicher@humanbite.org>
2014-01-25 19:18:52 +0100

    all of bombs

---
+++ Assets/Scripts/BombScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision c){
+		if (c.gameObject.tag != "Arrow"){
+			Destroy (c.collider.transform.root.gameObject);
+			Destroy (this.gameObject);
+		}
+	}
+}
