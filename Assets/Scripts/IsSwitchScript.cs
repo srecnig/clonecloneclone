26a3919be696078c1029b794c149bb13afcfbedd
srecnig <srecnig@fsinf.at>
2014-01-25 21:13:56 +0100

    switch the stuff

---
+++ Assets/Scripts/IsSwitchScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsSwitchScript : MonoBehaviour {
+
+	public string isSwitchFor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision c) {
+		// only if the player or a clone pressed it.
+		Debug.Log("PRESSED SWITCH!");
+	}
+
+	void OnCollisionExit(Collision c) {
+		Debug.Log("LEFT SWITCH!");
+	}
+}
