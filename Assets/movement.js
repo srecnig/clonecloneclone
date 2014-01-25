5eeccc9e3d0eb35e080b19b5872b638e91acf130
Apocrypt <westreicher@humanbite.org>
2014-01-25 11:27:27 +0100

    changed movement

--- Assets/movement.js
+++ Assets/movement.js
@@ -9,12 +9,12 @@ if (Input.GetKeyDown(KeyCode.W)) {
 		transform.Translate(0,1,0);
 	}
 	if (Input.GetKeyDown(KeyCode.A)) {
-		transform.Translate(-1,0,0);
+		transform.Rotate(0,0,90);
 	}
 	if (Input.GetKeyDown(KeyCode.S)) {
 		transform.Translate(0,-1,0);
 	}
 	if (Input.GetKeyDown(KeyCode.D)) {
-		transform.Translate(1,0,0);
+		transform.Rotate(0,0,-90);
 	}
 }
