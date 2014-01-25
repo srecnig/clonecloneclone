026ea06523779f8def5e5184565163a4f067d5cd
LieroLarryfier <steindlmatthias@gmx.at>
2014-01-25 23:10:03 +0100

    animation!!!

--- Assets/Scripts/ShootScript.cs
+++ Assets/Scripts/ShootScript.cs
@@ -15,12 +15,18 @@ public class ShootScript : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			// Debug.Log("SHOOT!" + this.gameObject.tag);
 			// instantiate arrow prefab
+			GameObject animation = Instantiate(Resources.Load("arrow_animation_0")) as GameObject;
 			GameObject arrow = Instantiate(Resources.Load("Arrow")) as GameObject; 
 			Vector3 pos = this.cloneTransform.position;
+			animation.transform.position = pos;
+			animation.transform.rotation = this.cloneTransform.rotation;
+			animation.transform.Rotate(0,0,90);
+			animation.transform.Translate(-1,0.5f,-0.3f);
 			arrow.transform.position = pos;
 			arrow.transform.rotation = this.cloneTransform.rotation;
 			arrow.transform.Translate (0, 0.8F, 0);
 			arrow.AddComponent("ArrowScript");
+
 		}
 	}
 }
