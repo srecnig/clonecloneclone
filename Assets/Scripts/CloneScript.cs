49684a7f479daa66c22aebc70b0087548e116620
mux <mux@darla.local>
2014-01-25 14:24:02 +0100

    added cloning to neutrals

---
+++ Assets/Scripts/CloneScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		string tag = collision.gameObject.tag;
+		if (tag == "Arrow") {
+			Debug.Log("COLLISION with: " +  collision.gameObject.tag);
+			if (this.gameObject.GetComponent("movement") == null) {
+				this.gameObject.AddComponent("movement");
+			}
+		}
+		
+	}
+}
