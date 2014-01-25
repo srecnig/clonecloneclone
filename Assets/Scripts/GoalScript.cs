0c6ac64a36fb195adecf701c4a0d5d27b3fefc0c
srecnig <srecnig@fsinf.at>
2014-01-25 13:19:44 +0100

    it's se working! (collissions. i guess)

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	GameObject.FindGameObjectsWithTag("Player")
+	}
+
+	void OnCollisionEnter(Collision c) {
+		Debug.Log("collission!");
+	}
+}
