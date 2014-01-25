506b9af475685ef33f615520b0b0deda16a01fbf
Apocrypt <westreicher@humanbite.org>
2014-01-25 12:08:06 +0100

    added neutral and boundaries

---
+++ Assets/Scripts/boundaries.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if (transform.position.y > 10) {
+			transform.position.y = 9.5;
+		}
+	if (transform.position.y < 0) {
+			transform.position.y = 0.5;
+		}
+	if (transform.position.x > 10) {
+			transform.position.x = 9.5;
+		}
+	if (transform.position.x < 0) {
+			transform.position.x = 0.5;
+		}
+}
---
+++ Assets/Scripts/movement.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+if (Input.GetKeyDown(KeyCode.W)) {
+		transform.Translate(0,1,0);
+	}
+if (Input.GetKeyDown(KeyCode.A)) {
+		transform.Rotate(0,0,90);
+	}
+if (Input.GetKeyDown(KeyCode.S)) {
+		transform.Translate(0,-1,0);
+	}
+if (Input.GetKeyDown(KeyCode.D)) {
+		transform.Rotate(0,0,-90);
+	}
+}
