0c243c96b4d5f774514155e71a1a9f40b3ef64e1
Apocrypt <westreicher@humanbite.org>
2014-01-25 17:22:54 +0100

    wall the hell u doing?

---
+++ Assets/WallCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision c){
+		if (Input.GetKey(KeyCode.W)) {
+			c.gameObject.transform.parent.Translate(0,-1,0);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			c.gameObject.transform.parent.Translate(0,1,0);
+		}
+	}
+}
