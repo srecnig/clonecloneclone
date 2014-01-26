82cb971c1d4a49e81042586264067d88510401a1
Apocrypt <westreicher@humanbite.org>
2014-01-26 01:06:02 +0100

    shuting down

---
+++ Assets/CloneImplodeScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloneImplodeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision c){
+		if (c.gameObject.name != "wall"){
+			if (this.gameObject.tag == "neutral" && c.gameObject.tag == "Arrow")
+			Destroy (c.collider.transform.root.gameObject);
+			Destroy (this.gameObject);
+		}
+	}
+}
